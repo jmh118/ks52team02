c624ed0ebdc9d500922dd70d74d62b3e0d3e8cbb
LeeSHerald <makecheck@naver.com>
2024-10-16 17:34:10 +0900

    [ks52team02(TTT) - LSH] 관리자-탈퇴회원조회기능 수정 중 detail : 탈퇴회원조회기능 수정 중

--- ks52team02/src/main/java/ks52team02/manager/member/mapper/ManagerMemberMapper.java
+++ ks52team02/src/main/java/ks52team02/manager/member/mapper/ManagerMemberMapper.java
@@ -3,10 +3,14 @@ package ks52team02.manager.member.mapper;
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import ks52team02.manager.member.dto.Member;
+import ks52team02.manager.member.dto.WithdrawalMember;
 
 @Mapper
 public interface ManagerMemberMapper {
 
 	// 관리자 - 전체 회원 조회
 	List<Member> getMemberList();
+	
+	// 관리자 - 탈퇴 회원 조회	
+	List<WithdrawalMember> getWithdrawalMemberList();
 }
--- ks52team02/src/main/java/ks52team02/manager/member/service/ManagerMemberServiceImpl.java
+++ ks52team02/src/main/java/ks52team02/manager/member/service/ManagerMemberServiceImpl.java
@@ -3,8 +3,9 @@ package ks52team02.manager.member.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
-import org.springframework.ui.Model;
+
 import ks52team02.manager.member.dto.Member;
+import ks52team02.manager.member.dto.WithdrawalMember;
 import ks52team02.manager.member.mapper.ManagerMemberMapper;
 import lombok.RequiredArgsConstructor;
 
@@ -12,12 +13,19 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ManagerMemberServiceImpl implements ManagerMemberService {
 
-	private final ManagerMemberMapper memberMapper;
+	private final ManagerMemberMapper managerMemberMapper;
 
 	@Override
 	public List<Member> getMemberList(){
-		List<Member> memberList = memberMapper.getMemberList();
+		List<Member> memberList = managerMemberMapper.getMemberList();
 		
 		return memberList;
+	}
+
+	@Override
+	public List<WithdrawalMember> getWithdrawalMemberList() {
+		List<WithdrawalMember> withdrawalMemberList = managerMemberMapper.getWithdrawalMemberList();
+		
+		return null;
 	}
 }
