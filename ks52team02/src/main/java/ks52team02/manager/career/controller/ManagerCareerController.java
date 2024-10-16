3b00830ed5d512a8de37ffffd2ad0883dedcfb53
PC_1M <PC_1M@DESKTOP-ND7F2O5>
2024-10-16 23:07:26 +0900

    [ksmart-KKM] 마이바티스가 연결?이 인됨 detail: 찾아보니 마이바티스 연결이 안되는것 같음

--- ks52team02/src/main/java/ks52team02/manager/career/controller/ManagerCareerController.java
+++ ks52team02/src/main/java/ks52team02/manager/career/controller/ManagerCareerController.java
@@ -1,23 +1,33 @@
 package ks52team02.manager.career.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import ks52team02.manager.career.mapper.WorkMapper;
+import ks52team02.manager.career.dto.Career;
 import ks52team02.manager.career.service.CareerService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/manager/career")
+@Slf4j
 public class ManagerCareerController {
 
 	private final CareerService careerService;
 	
 	@GetMapping("/work")
-    public String mentorCareerWork() {
+    public String mentorCareerWork(Model model) {
         System.out.println("멘토 근무경력 승인 페이지 이동");
+        
+        List<Career> memberList = careerService.getMemberCareer();
+        
+        model.addAttribute("memberList", memberList);
+        
         return  "manager/career/workApprove";
     }
 
--- ks52team02/src/main/java/ks52team02/manager/career/service/CareerServiceImpl.java
+++ ks52team02/src/main/java/ks52team02/manager/career/service/CareerServiceImpl.java
@@ -3,6 +3,7 @@ package ks52team02.manager.career.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import ks52team02.manager.career.dto.Career;
 import ks52team02.manager.career.mapper.WorkMapper;
@@ -10,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class CareerServiceImpl implements CareerService {
 
 	private final WorkMapper workMapper;
