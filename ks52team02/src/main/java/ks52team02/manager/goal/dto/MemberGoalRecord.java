b5f5dded03985ee430715aafa886a1fdf3fb3493
jmh118 <nnyzip@gmail.com>
2024-10-22 00:01:24 +0900

    [ks52team02(TTT) - JMH] 관리자 멘티 목표 관리 수정 detail : 관리자 멘티 목표 관리 관련해서 mapper.xml 수정

--- ks52team02/src/main/java/ks52team02/manager/goal/dto/MemberGoalRecord.java
+++ ks52team02/src/main/java/ks52team02/manager/goal/dto/MemberGoalRecord.java
@@ -5,9 +5,20 @@ import lombok.Data;
 @Data
 public class MemberGoalRecord {
 	
+	private String goalRecordCode;
+	private String goalCode;
 	private String memberId;
+	private String goalAddDate;
 	private String goalRecordTitle;
 	private String goalRecordContent;
+	private String goalRecordBeginningTime;
+	private String goalRecordEndTime;
+	private String goalRecordHour;
+	private String goalRecordACMLHour;
+	private String goalRecordACMLCnt;
 	private String goalRecordAddDate;
 	
+	
+	private MemberGoal goal;    
+	private GoalTopic topic;
 }
