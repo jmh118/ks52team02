7835aef427286cdfc20f260ea6464811036a9d06
jmh118 <nnyzip@gmail.com>
2024-10-22 03:10:15 +0900

    [ks52team02(TTT) - JMH] 관리자 명예멘토 조회 완성 detail : 관리자 명예멘토 조회

--- ks52team02/src/main/java/ks52team02/manager/honor/controller/ManagerHonorController.java
+++ ks52team02/src/main/java/ks52team02/manager/honor/controller/ManagerHonorController.java
@@ -1,14 +1,24 @@
 package ks52team02.manager.honor.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import ks52team02.manager.honor.mapper.ManagerHonorMapper;
+import ks52team02.manager.member.dto.Member;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
+@RequiredArgsConstructor
 @Controller
 @RequestMapping("/manager/honor")
 public class ManagerHonorController {
 	
+	private final ManagerHonorMapper managerHonorMapper;
 	
 	@GetMapping("/criteriaList")
 	public String managerHonoraryMentorCriteriaList() {
@@ -17,8 +27,17 @@ public class ManagerHonorController {
 	}
 	
 	@GetMapping("/list")
-	public String managerHonoraryMentorList() {
+	public String managerHonoraryMentorList(Model model) {
 		System.out.println("관리자 명예멘토 조회 화면");
+		
+		List<String> honorMentorIdList = managerHonorMapper.getHonorMentorIdList();
+		List<Member> honorMentorList = managerHonorMapper.getHornorMentorList(honorMentorIdList);
+		
+		log.info("잘 받?? : {}", honorMentorList);
+		
+		model.addAttribute("title", "명에 멘토 조회");
+		model.addAttribute("honorMentorList", honorMentorList);
+		
 		return  "manager/honor/honoraryMentorList";
 	}
 
