498b49563b5f383f2d1c2006e1fc2d5cbeb28c9a
jmh118 <nnyzip@gmail.com>
2024-10-22 22:58:44 +0900

    [ks52team02(TTT) - JMH] 관리자 명예멘토 기준 조회 detail : 관리자 명예멘토 기준 조회 완성

--- ks52team02/src/main/java/ks52team02/manager/honor/controller/ManagerHonorController.java
+++ ks52team02/src/main/java/ks52team02/manager/honor/controller/ManagerHonorController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import ks52team02.manager.honor.mapper.ManagerHonorMapper;
 import ks52team02.manager.member.dto.Member;
+import ks52team02.manager.review.dto.MentorReviews;
+import ks52team02.manager.review.mapper.ManagerReviewMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -19,21 +21,25 @@ import lombok.extern.slf4j.Slf4j;
 public class ManagerHonorController {
 	
 	private final ManagerHonorMapper managerHonorMapper;
+	private final ManagerReviewMapper managerReviewMapper;
 	
 	@GetMapping("/criteriaList")
-	public String managerHonoraryMentorCriteriaList() {
-		System.out.println("관리자 명예멘토 기준 조회 내역 화면");
+	public String managerHonoraryMentorCriteriaList(Model model) {
+		
+		List<MentorReviews> mentorReviewsDataList = managerReviewMapper.getMentorReviewsDataList();
+		
+		model.addAttribute("title", "명예멘토 기준 조회");
+		model.addAttribute("content", "멘티의 명예멘토 기준 (평점 평균과 평점 개수, 플랫폼 내 기준) 충족 시 승인 버튼 활성화 후 명예멘토로 승인");		
+		model.addAttribute("mentorReviewsDataList", mentorReviewsDataList);
+		
 		return  "manager/honor/honoraryMentorCriteriaList";
 	}
 	
 	@GetMapping("/list")
 	public String managerHonoraryMentorList(Model model) {
-		System.out.println("관리자 명예멘토 조회 화면");
 		
 		List<String> honorMentorIdList = managerHonorMapper.getHonorMentorIdList();
 		List<Member> honorMentorList = managerHonorMapper.getHornorMentorList(honorMentorIdList);
-		
-		log.info("잘 받?? : {}", honorMentorList);
 		
 		model.addAttribute("title", "명에 멘토 조회");
 		model.addAttribute("honorMentorList", honorMentorList);
--- ks52team02/src/main/java/ks52team02/manager/review/mapper/ManagerReviewMapper.java
+++ ks52team02/src/main/java/ks52team02/manager/review/mapper/ManagerReviewMapper.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import ks52team02.manager.review.dto.MentorReviews;
 import ks52team02.manager.review.dto.Review;
 
 @Mapper
 public interface ManagerReviewMapper {
+	
+	// 멘토별 후기 관련해서 통계 조회
+	List<MentorReviews> getMentorReviewsDataList();
 
 	// 전체 후기 조회
 	List<Review> getAllMentoringReviewList();
