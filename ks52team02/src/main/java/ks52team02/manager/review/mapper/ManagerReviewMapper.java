3b72ac74d92a10717daa5d6d30410c7e4ca9ec49
jmh118 <nnyzip@gmail.com>
2024-10-24 14:47:09 +0900

    [ks52team02(TTT) - JMH] 관리자 멘토링 후기 삭제 detail :  관리자 멘토링 후기 삭제 완

--- ks52team02/src/main/java/ks52team02/manager/review/mapper/ManagerReviewMapper.java
+++ ks52team02/src/main/java/ks52team02/manager/review/mapper/ManagerReviewMapper.java
@@ -10,6 +10,9 @@ import ks52team02.manager.review.dto.Review;
 @Mapper
 public interface ManagerReviewMapper {
 	
+	// 관리자가 후기 삭제 
+	int removeReview(String reviewCode);
+	
 	// 멘토별 후기 관련해서 통계 조회
 	List<MentorReviews> getMentorReviewsDataList();
 
---
+++ ks52team02/src/main/java/ks52team02/manager/review/service/ManagerReviewService.java
@@ -0,0 +1,7 @@
+package ks52team02.manager.review.service;
+
+public interface ManagerReviewService {
+	
+	void removeReview(String reviewCode);
+
+}
