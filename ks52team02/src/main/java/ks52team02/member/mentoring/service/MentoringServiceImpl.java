0431bac073d9f2afab3cfa7c154656c2f1856067
yonguuu131 <wo1317@naver.com>
2024-10-25 16:42:03 +0900

    [ks52team02(TTT) - JYW] apply추가 detail : insert추가

--- ks52team02/src/main/java/ks52team02/member/mentoring/service/MentoringServiceImpl.java
+++ ks52team02/src/main/java/ks52team02/member/mentoring/service/MentoringServiceImpl.java
@@ -5,7 +5,8 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import jakarta.servlet.http.HttpSession;
+import ks52team02.common.mapper.CommonMapper;
+import ks52team02.member.mentoring.dto.MentoringApply;
 import ks52team02.member.mentoring.dto.Notice;
 import ks52team02.member.mentoring.dto.NoticeAnswer;
 import ks52team02.member.mentoring.dto.NoticeDetail;
@@ -13,18 +14,29 @@ import ks52team02.member.mentoring.dto.NoticeQuestion;
 import ks52team02.member.mentoring.dto.Topic;
 import ks52team02.member.mentoring.mapper.MentoringMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
+@Slf4j
 public class MentoringServiceImpl implements MentoringService{
 	
 	private final MentoringMapper mentoringMapper;
+	private final CommonMapper commonMapper;
+	
+	@Override
+	public void addMentoringApply(MentoringApply mentoringApply) {
+		String nextCode = commonMapper.getPrimaryKey("mentoring_apply", "mentoring_apply_code", "mentoring_apply_code_");
+		mentoringApply.setApplyCode(nextCode);
+		log.info("mentoringApply : {}",mentoringApply);
+		mentoringMapper.addMentoringApply(mentoringApply);
+	}
 	
 	@Override
 	public void addNoticeAnswer(NoticeAnswer noticeAnswer) {
-		String nextCode = mentoringMapper.getNextNoticeAnswerCode();
-		noticeAnswer.setAnswerCode("mentoring_notice_question_answer_code_" + nextCode);
+		String nextCode = commonMapper.getPrimaryKey("mentoring_notice_question_answer","mentoring_notice_question_answer_code","mentoring_notice_question_answer_code_");
+		noticeAnswer.setAnswerCode(nextCode);
 		mentoringMapper.addNoticeAnswer(noticeAnswer);
 		
 	}
@@ -32,8 +44,8 @@ public class MentoringServiceImpl implements MentoringService{
 	@Override
 	public void addNoticeQuestion(NoticeQuestion noticeQuestion) {
 		//코드추가
-		String nextCode = mentoringMapper.getNextNoticeQuestionCode();
-		noticeQuestion.setQuestionCode("mentoring_notice_question_code_" + nextCode);
+		String nextCode = commonMapper.getPrimaryKey("mentoring_notice_question", "mentoring_notice_question_code", "mentoring_notice_question_code_");
+		noticeQuestion.setQuestionCode(nextCode);
 		mentoringMapper.addNoticeQuestion(noticeQuestion);
 	}
 	
@@ -61,8 +73,8 @@ public class MentoringServiceImpl implements MentoringService{
 	
 	@Override
 	public void addNotice(Notice notice) {
-		String nextCode = mentoringMapper.getNextNoticeCode();
-		notice.setNoticeCode("mentoring_notice_code_" + nextCode);
+		String nextCode = commonMapper.getPrimaryKey("mentoring_notice", "mentoring_notice_code", "mentoring_notice_code_");
+		notice.setNoticeCode(nextCode);
 		
 		mentoringMapper.addNotice(notice);		
 	}
