ddd345e0eefcd36f2ded7f5c1f7f4a4818bcbc13
jmh118 <nnyzip@gmail.com>
2024-11-01 16:44:34 +0900

    [ks52team02 - JMH] 관리자 명예 멘토 승인 및 멘토 정산 승인 detail : 관리자 명예 멘토 승인 및 멘토 정산 승인

--- ks52team02/src/main/java/ks52team02/manager/honor/controller/ManagerHonorController.java
+++ ks52team02/src/main/java/ks52team02/manager/honor/controller/ManagerHonorController.java
@@ -6,11 +6,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import ks52team02.manager.honor.mapper.ManagerHonorMapper;
+import ks52team02.manager.honor.service.ManagerHonorService;
 import ks52team02.manager.member.dto.Member;
-import ks52team02.manager.review.dto.MentorReviews;
-import ks52team02.manager.review.mapper.ManagerReviewMapper;
+import ks52team02.manager.review.dto.MentorReviewData;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -20,26 +21,41 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/manager/honor")
 public class ManagerHonorController {
 	
-	private final ManagerHonorMapper managerHonorMapper;
-	private final ManagerReviewMapper managerReviewMapper;
+	private final ManagerHonorService managerHonorService; 
+	
+	@GetMapping("/approve")
+	@ResponseBody
+	public String hornoMentorApprove(@RequestParam(name="memberId") String memberId) {
+		
+		managerHonorService.honorMentorApprove(memberId);
+		
+		return "redirect:/manager/honor/criteriaList";
+	}
+	
+	@GetMapping("/cancel")
+	@ResponseBody
+	public String hornoMentorCancel(@RequestParam(name="memberId") String memberId) {
+		
+		managerHonorService.honorMentorCancel(memberId);
+		
+		return "redirect:/manager/honor/criteriaList";
+	}
 	
 	@GetMapping("/criteriaList")
-	public String managerHonoraryMentorCriteriaList(Model model) {
+	public String getMentorReviewsDataList(Model model) {
 		
-		List<MentorReviews> mentorReviewsDataList = managerReviewMapper.getMentorReviewsDataList();
+		List<MentorReviewData> mentorReviewsDataList = managerHonorService.getMentorReviewsDataList();
 		
 		model.addAttribute("title", "명예멘토 기준 조회");
-		model.addAttribute("content", "멘티의 명예멘토 기준 (평점 평균과 평점 개수, 플랫폼 내 기준) 충족 시 승인 버튼 활성화 후 명예멘토로 승인");		
 		model.addAttribute("mentorReviewsDataList", mentorReviewsDataList);
 		
 		return  "manager/honor/honoraryMentorCriteriaList";
 	}
 	
 	@GetMapping("/list")
-	public String managerHonoraryMentorList(Model model) {
+	public String getHonorMentorList(Model model) {
 		
-		List<String> honorMentorIdList = managerHonorMapper.getHonorMentorIdList();
-		List<Member> honorMentorList = managerHonorMapper.getHornorMentorList(honorMentorIdList);
+		List<Member> honorMentorList = managerHonorService.getHornorMentorList();
 		
 		model.addAttribute("title", "명에 멘토 조회");
 		model.addAttribute("honorMentorList", honorMentorList);
--- ks52team02/src/main/java/ks52team02/manager/honor/mapper/ManagerHonorMapper.java
+++ ks52team02/src/main/java/ks52team02/manager/honor/mapper/ManagerHonorMapper.java
@@ -4,11 +4,21 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import ks52team02.manager.honor.dto.CriteriaHonorMentor;
 import ks52team02.manager.member.dto.Member;
 
 
 @Mapper
 public interface ManagerHonorMapper {
+	
+	// 명예멘토 취소 
+	int honorMentorCancel(String memeberId);
+	
+	// 명예멘토 승인
+	int honorMentorApprove(String memeberId);
+	
+	// 명예멘토 기준 조회
+	CriteriaHonorMentor getCriteriaHonorMentor();
 	
 	// 명예멘토 리스트 조회
 	List<Member> getHornorMentorList(List<String> mentorId);
