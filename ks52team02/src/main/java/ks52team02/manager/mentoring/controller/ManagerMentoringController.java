d6c3ea736af878013e132b0e0361e4943901eab6
yonguuu131 <wo1317@naver.com>
2024-11-06 15:59:05 +0900

    [ks52team02 - JYW] MemberMain 창모드 메뉴추가, 관리자 멘토링메뉴추가 detail : 관리자페이지 멘토링 질문,답변,신청 페이지 추가

--- ks52team02/src/main/java/ks52team02/manager/mentoring/controller/ManagerMentoringController.java
+++ ks52team02/src/main/java/ks52team02/manager/mentoring/controller/ManagerMentoringController.java
@@ -4,9 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import ks52team02.manager.mentoring.dto.ManagerMetoringNotice;
 import ks52team02.manager.mentoring.service.ManagerMentoringService;
+import ks52team02.member.mentoring.dto.MentoringApply;
+import ks52team02.member.mentoring.dto.NoticeAnswer;
+import ks52team02.member.mentoring.dto.NoticeQuestion;
 import ks52team02.page.PageInfo;
 import ks52team02.page.Pageable;
 import lombok.RequiredArgsConstructor;
@@ -20,18 +24,55 @@ public class ManagerMentoringController {
 	
 	private final ManagerMentoringService managerMentoringService;
 	
+	@GetMapping("/applyList")
+	public String getManagerMentoringApplyList(Pageable pageable, Model model) {
+		
+		PageInfo<MentoringApply> applyList = managerMentoringService.getManagerMentoringApplyList(pageable);
+		model.addAttribute("applyList", applyList);
+		return "manager/mentoring/noticeApplyList";
+	}
+	
+	@GetMapping("/removeQuestion")
+	public String removeQuestions(@RequestParam(name="questionCode") String questionCode, Model model) {
+		managerMentoringService.removeQuestion(questionCode);
+		return "redirect:/manager/mentoring/questionList";
+	}
+	
+	@GetMapping("/removeAnswer")
+	public String removeAnswers(@RequestParam(name="answerCode") String answerCode, Model model) {
+		managerMentoringService.removeAnswer(answerCode);
+		
+		return "redirect:/manager/mentoring/answerList";
+	}
+	
 	@GetMapping("/noticeList")
     public String moveManagerNoticeList(Pageable pageable,Model model) {
 		
 		PageInfo<ManagerMetoringNotice> noticeList = managerMentoringService.getManagerNoticeList(pageable);
 		
-		//List<ManagerMetoringNotice> noticeList = managerMentoringService.getManagerNoticeList();
     	model.addAttribute("noticeList", noticeList);
 		
 		System.out.println("멘토링 공고 조회 페이지 이동");
         return  "manager/mentoring/noticeList";
     }
 
-
+	@GetMapping("/questionList")
+	public String moveManagerNoticeQuestionList(Pageable pageable, Model model) {
+		
+		PageInfo<NoticeQuestion> questionList = managerMentoringService.getManagerNoticeQuestionList(pageable);
+		
+		model.addAttribute("questionList", questionList);
+		
+		return "manager/mentoring/noticeQuestionList";
+	}
+	
+	@GetMapping("/answerList")
+	public String moveManagerNoticeAnswerList(Pageable pageable, Model model) {
+		
+		PageInfo<NoticeAnswer> answerList = managerMentoringService.getManagerNoticeAnswerList(pageable);
+		
+		model.addAttribute("answerList", answerList);
+		return "manager/mentoring/noticeAnswerList";
+	}
 	
 }
