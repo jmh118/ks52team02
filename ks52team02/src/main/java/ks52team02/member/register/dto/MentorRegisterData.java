e19f9c39093d8c3ccca483c43c86e52fc864fb38
LeeSHerald <makecheck@naver.com>
2024-11-11 17:32:34 +0900

    [TTT - LSH] 회원가입 및 파일업로드/다운로드 수정 중

---
+++ ks52team02/src/main/java/ks52team02/member/register/dto/MentorRegisterData.java
@@ -0,0 +1,14 @@
+package ks52team02.member.register.dto;
+
+import java.util.List;
+import ks52team02.member.mypage.dto.MentorWork;
+import lombok.Data;
+
+
+@Data
+public class MentorRegisterData {
+	
+	private List<MentorWork> workData;
+	
+
+}
